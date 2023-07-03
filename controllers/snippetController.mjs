d481447900d0d0c7a64f2173beeb2b56b7990f0b
Alan <56948680+waterrmalann@users.noreply.github.com>
2023-07-03 13:01:02 +0530

    ⚙️snippetController.js

---
+++ controllers/snippetController.mjs
@@ -0,0 +1,35 @@
+/* 
+    At the speed of 1000 IDs/second, 
+    ~7 thousand years or 63B IDs needed, in order to have a 1% probability of at least one collision.
+    According to: https://zelark.github.io/nano-id-cc/
+*/
+import { customAlphabet } from 'nanoid';
+const alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+const nanoid = customAlphabet(alphabet, 13);
+
+import Snippet from '../models/Snippet.mjs';
+
+// Create a new snippet
+const createSnippet = async (req, res) => {
+    const uniqueID = nanoid();
+    const value = req.body.value;
+    try {
+        const snippet = await Snippet.create({ value, uniqueID });
+        res.redirect(`/${snippet.uniqueID}`);
+    } catch (e) {
+        res.render('new', { value });
+    }
+};
+
+// Retrieve a snippet by its ID.
+const getSnippetById = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const snippet = await Snippet.findOne({ uniqueID: id });
+        res.render('snippet', { code: snippet.value });
+    } catch (e) {
+        res.redirect('/');
+    }
+};
+
+export { createSnippet, getSnippetById }
